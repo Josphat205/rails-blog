7475e993733ff98a1f87847555d3ed50580f8971
lomantech <87063237+lomantech@users.noreply.github.com>
2022-11-23 16:02:21 +0300

    Add method to increase counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,11 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
   validates :text, presence: true
+
+  after_save :update_counter
+
+
+  def update_counter
+    post.increment!(:commentsCounter)
+  end
 end
