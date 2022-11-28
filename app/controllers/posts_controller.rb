d1bdf078228505a96fcd11fb858474c2328a5714
lomantech <87063237+lomantech@users.noreply.github.com>
2022-11-28 11:56:01 +0300

    linters config

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
-    def index
-        @user = User.find(params[:user_id])
-        @posts = @user.posts.all
-    end
-    def show
-        @post = Post.find(params[:id])
-    end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
 end
