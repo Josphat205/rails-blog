74cab111bf8c604e55a898db2e59a50fe964059f
lomantech <87063237+lomantech@users.noreply.github.com>
2022-11-29 12:12:49 +0300

    Add users controllers tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Users', type: %w[request feature] do
+  before :each do
+    @user = User.create(
+      name: 'Waleed',
+      bio: 'Web Developer',
+      photo: 'http://waleed.png',
+      posts_counter: 0
+    )
+  end
+
+  it 'renders the index template' do
+    get '/users/'
+    expect(response.status).to eq(200)
+    expect(response).to render_template('index')
+  end
+
+  it 'renders the show template' do
+    get "/users/#{@user.id}"
+    expect(response.status).to eq(200)
+    expect(response).to render_template('show')
+  end
+
+  it 'renders index template with right text' do
+    visit '/users/'
+    expect(page).to have_text('users')
+  end
+
+  it 'renders show template with right text' do
+    visit "/users/#{@user.id}"
+    expect(page).to have_text('Sigle user goes here')
   end
 end
