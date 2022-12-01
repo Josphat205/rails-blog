b6b3e510743324e2c4f3bd18f5a5fd5083c01d5f
lomantech <87063237+lomantech@users.noreply.github.com>
2022-12-01 13:11:24 +0300

    linters config

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users do
     resources :posts do
-      resources :comments, only: [:new,:create]
+      resources :comments, only: %i[new create]
       resources :likes, only: [:create]
     end
   end
