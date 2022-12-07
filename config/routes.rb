7561ab2fa4f1ec9f9b9ac2b6bb998b8b8b654008
Kehinde Onifade <onifkay@gmail.com>
2022-12-07 16:03:11 +0100

    implement og out functionality

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
     end
   end
  
+  devise_scope  :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
+  
   # Defines the root path route ("/")
   root 'users#index'
 end
