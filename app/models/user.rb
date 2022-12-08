c8a193e64378ebc638f73a9b74b8c972090da353
lomantech <87063237+lomantech@users.noreply.github.com>
2022-12-08 13:41:15 +0300

    file update after installing cancan

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable
   validates :name, :photo, :bio, presence: true
-  has_many :posts, foreign_key: 'author_id'
-  has_many :comments, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
+  has_many :posts, foreign_key: 'author_id', :dependent => :destroy
+  has_many :comments, foreign_key: 'author_id', :dependent => :destroy
+  has_many :likes, foreign_key: 'author_id', :dependent => :destroy
   before_validation :default_values
 
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
